6d6b2b78e3e72ce4129d6a623e25f217cca59367
swayamraina <swayamraina@gmail.com>
2020-05-18 00:09:06 +0530

    removed meta and added base constructors

--- src/main/java/dev/swayamraina/signal/group/core/signal/Signal.java
+++ src/main/java/dev/swayamraina/signal/group/core/signal/Signal.java
@@ -1,12 +1,17 @@
 package dev.swayamraina.signal.group.core.signal;
 
-import dev.swayamraina.signal.group.entity.ExtractorKey;
+import dev.swayamraina.signal.group.core.extractor.ExtractorKey;
 import dev.swayamraina.signal.group.core.http.Http;
-import dev.swayamraina.signal.group.entity.Meta;
 
 import java.util.List;
+import java.util.Optional;
+
+import static dev.swayamraina.signal.group.utils.Constants.EMPTY;
 
 public final class Signal {
+
+    private String name;
+    public String name () { return name; }
 
     private int priority;
     public int priority () { return priority; }
@@ -17,10 +22,32 @@ public final class Signal {
     private List<Signal> children;
     public List<Signal> children () { return children; }
 
-    private Meta meta;
-    public Meta meta () { return meta; }
-
     private List<ExtractorKey> keys;
     public List<ExtractorKey> keys () { return keys; }
+
+    private Optional<String> description;
+    private String description () { return description.isPresent() ? description.get() : EMPTY; }
+    public void withDescription (String description) {
+        this.description = Optional.of(description == null ? EMPTY : description);
+    }
+
+
+
+
+    public Signal (String name, int priority, Http http, List<Signal> children, List<ExtractorKey> keys) {
+        if (null == children || null == keys)
+            throw new IllegalArgumentException("non nullable properties");
+        if (0 > priority)
+            throw new IllegalArgumentException("priority should be > 0");
+        if (null == name || EMPTY.equals(name))
+            throw new IllegalArgumentException("signal name cannot be null/empty");
+
+        this.name = name;
+        this.priority = priority;
+        this.http = http;
+        this.children = children;
+        this.keys = keys;
+        this.withDescription(EMPTY);
+    }
 
 }
--- src/main/java/dev/swayamraina/signal/group/core/signal/SignalGroup.java
+++ src/main/java/dev/swayamraina/signal/group/core/signal/SignalGroup.java
@@ -1,26 +1,31 @@
 package dev.swayamraina.signal.group.core.signal;
 
 import dev.swayamraina.signal.group.annotations.Internal;
-import dev.swayamraina.signal.group.entity.Meta;
 
 import java.util.List;
+import java.util.Optional;
+
+import static dev.swayamraina.signal.group.utils.Constants.EMPTY;
 
 public final class SignalGroup {
 
     @Internal private String uid;
     public String uid () { return uid; }
 
-    private Meta meta;
-    public Meta meta () { return meta; }
-
     private List<Signal> signals;
     public List<Signal> signals () { return signals; }
 
+    private Optional<String> description;
+    private String description () { return description.isPresent() ? description.get() : EMPTY; }
+    public void withDescription (String description) {
+        this.description = Optional.of(description == null ? EMPTY : description);
+    }
 
 
-    public SignalGroup (Meta meta, List<Signal> signals) {
-        this.meta = meta;
+
+    public SignalGroup (List<Signal> signals) {
         this.signals = signals;
+        this.withDescription(EMPTY);
     }
 
 }
