7671315632bb8434139084dc3de4dcc4a431fc74
swayamraina <swayamraina@gmail.com>
2020-05-20 08:26:23 +0530

    add request start time

--- src/main/java/dev/swayamraina/signal/group/core/executor/ExecuteRequest.java
+++ src/main/java/dev/swayamraina/signal/group/core/executor/ExecuteRequest.java
@@ -1,12 +1,20 @@
 package dev.swayamraina.signal.group.core.executor;
 
 
+import dev.swayamraina.signal.group.annotations.Internal;
+
 public class ExecuteRequest {
+
+    @Internal private long start;
+    public long start () { return start; }
 
     private String uid;
     public String uid () { return uid; }
 
 
-    public ExecuteRequest (String uid) { this.uid = uid; }
+    public ExecuteRequest (String uid) {
+        this.uid = uid;
+        this.start = System.nanoTime();
+    }
 
 }
